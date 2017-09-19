0cc8284f35c20f5ec5e97a4f4f5bb52229525bd0
garr1s0n <gary.wilpizeski@gmail.com>
2017-09-19 19:12:16 -0400

    Basic Express Server

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // App Credentials
 const credentials = require('./credentials.json')
+
+// App Requires
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+// App Start
+const app = express();
+
+// API Root
+app.get('/', (req, res) => {
+  res.send('API Root');
+});
+
+// Start Server
+app.listen(3000, () => console.log('Server started...'));
